a13fa7311fb3c62e5f82275e18167eb35de6c847
Христюк Дарья Алексеевна <@itstep.loc>
2022-11-14 17:42:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
 function App() {
+  const [password, setPassword] = useState('')
+  const [passwordDirty,setPasswordDirty] = useState(false)
+  const [passwordError,setPasswordError] = useState('пароль не может быть пустым')
+  const [formValid, setFormValid] = useState(false)
+  const blurHandler = (e) =>{
+    switch(e.target.name){
+      case 'password':
+        setPasswordDirty(true)
+        break
+    }
+  }
+  useEffect( ()=> {
+    if (passwordError) {
+      setFormValid(false)
+    }
+    else{
+      setFormValid(true)
+    }
+  }, [passwordError])
+  const passwordHandler = (e) =>{
+    setPassword(e.target.value)
+    if(e.target.value.length < 5){
+      setPasswordError('Пароль не может быть менее 5 символов')
+    }
+    else{
+      setPasswordError('')
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <h1>regist page</h1>
+        {(passwordError && passwordDirty) && <div style={{color:'red'}}>{passwordError}</div>}
+        <input onChange={e => passwordHandler(e)} value={password} onBlur={e => blurHandler(e)} name='password' type="text" placeholder='password...'/>
+        <button disabled={!formValid} type='submit'>Regist</button>
+      </form>
     </div>
   );
 }
-
 export default App;
